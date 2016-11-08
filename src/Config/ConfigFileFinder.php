439db40b23de561214ac409509f2e6f1eb19792c
Simon Bäumer <s.baeumer@shopware.com>
2016-11-08 09:30:29 +0100

    Prefer non distribution files in ConfigFileFinder

--- src/Config/ConfigFileFinder.php
+++ src/Config/ConfigFileFinder.php
@@ -17,12 +17,27 @@ class ConfigFileFinder
             $globResult = glob($currentDirectory . '/' . self::VALID_FILE_NAME_GLOB);
 
             if (count($globResult)) {
-                return $globResult[0];
+                return $this->preferNonDistributionFiles($globResult);
             }
 
             $currentDirectory = dirname($currentDirectory);
         } while ($currentDirectory !== '/');
 
         throw new \RuntimeException('No config file found, make sure you have created a .psh file');
+    }
+
+    /**
+     * @param string[] $configFiles
+     * @return string
+     */
+    private function preferNonDistributionFiles(array $configFiles): string
+    {
+        foreach ($configFiles as $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) !== 'dist') {
+                return $file;
+            }
+        }
+
+        return $configFiles[0];
     }
 }
