2e7aa4d22af03931d29ce105e0d50d1265189f32
Thomas Eiling <teiling88@gmail.com>
2017-10-24 14:58:50 +0200

    adding coverage generation

--- actions/unit.sh
+++ actions/unit.sh
@@ -2,5 +2,5 @@
 # DESCRIPTION: Execute unit tests
 
 bin/php-cs-fixer fix
-bin/phpunit --debug --verbose
+bin/phpunit --debug --verbose --coverage-clover=./build/coverage.xml
 ./humbug.phar --no-interaction
